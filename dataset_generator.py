97be204c1028de0d001a81458b74d07436efcd17
Brenno Faro <brenofaro97@hotmail.com>
2025-03-24 23:36:09 -0300

    Update dataset_generator.py

--- dataset_generator.py
+++ dataset_generator.py
@@ -21,7 +21,7 @@ def generate_qrcode(data, angle=0, size=1, dx=0, dy=0):
         version=size,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
-        border=0,
+        border=4,
     )
     qr.add_data(data)
     qr.make(fit=True)
